5dc7ab79733a4a42ffcc26586567a976ffc65dc4
dhivyabharathim <dhivyamariyappan19@gmail.com>
2019-06-06 20:28:52 +0530

    Add files via upload

---
+++ mathpow.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+import java.lang.*;
+class mathpow
+
+{
+	
+public static void main (String[] args) 
+	
+{
+		
+Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		
+int b=s.nextInt();
+		System.out.println(Math.pow(a,b));
+
+}
+}		
